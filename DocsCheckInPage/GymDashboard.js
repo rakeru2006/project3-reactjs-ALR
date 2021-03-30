21a1d9ccbd43a2ec4ef4001e137bef5eca0e35c4
Arturo Rodriguez <arturo-rito@Arturos-MacBook-Air.local>
2021-03-30 12:26:44 -0600

    Update the code so that admin can search for customer using membership ID
    -Can search for a customer using membership ID / phone number.
    
    -Can pull customer DoB and Member Name so that they can confirm identity.
    
    -Will display errors if number does not match or is inputted incorrectly.

--- DocsCheckInPage/GymDashboard.js
+++ DocsCheckInPage/GymDashboard.js
@@ -25,6 +25,13 @@ var clientList = [
 
 console.log(clientList.length);
 console.log(clientList[0].ID);
+
+membersIDs = []
+for (var i=0; i< clientList.length; i++) {
+  membersIDs.push(clientList[i].ID)
+}
+console.log(membersIDs)
+
 /* for
 var verifyID;
  */
@@ -61,8 +68,9 @@ $("#manualInput").click(function() {
   console.log( "i was clicked" );
   $("#clientSearchBox").css("display", "none");
   $("#manualInputBox").css("display", "block");
-  $("#scanQRBox").css("display","none");});
-  html5QrcodeScanner.clear();
+  $("#scanQRBox").css("display","none");
+  //html5QrcodeScanner.clear();
+});
 
 
 $("#clientSearch").click(function() {
@@ -70,15 +78,50 @@ $("#clientSearch").click(function() {
   $("#clientSearchBox").css("display", "block");
   $("#manualInputBox").css("display", "none");
   $("#scanQRBox").css("display","none");
-  html5QrcodeScanner.clear();
-
+  //html5QrcodeScanner.clear();
 });
 
 $("#searchMemberID").click(function() {
-  verifyID = $("#searchValue").val();
-  $("#memberIDSearchResult").css("display","block");
-//  if (verifyID !== )
+  $("#IDFoundConfirm").css("display","none")
+  $("#ManualNameConfirm").css("display","none")
+  $("#ManualDoBConfirm").css("display","none")
 
+
+  verifyID = $("#searchValue").val();
+  console.log(verifyID)
+  if(isNaN(verifyID)){
+    $("#memberIDSearchResult").css("display","block")
+    $("#IDmatchManualConfirm").addClass("text-danger")
+    $("#IDmatchManualConfirm").text("You inputted other characters other than numbers! Enter a valid ten digit memberID").
+    return;
+  } 
+  else if (!(verifyID >= 0)) {
+    $("#memberIDSearchResult").css("display","block")
+    $("#IDmatchManualConfirm").addClass("text-danger")
+    $("#IDmatchManualConfirm").text("You inputted a negative number. This ID is invalid. Insert a valid ten digit ID number").
+    return;
+  }
+  else if (verifyID.length !== 10) {
+    $("#memberIDSearchResult").css("display","block")
+    $("#IDmatchManualConfirm").addClass("text-danger")
+    $("#IDmatchManualConfirm").text("This is only has " + verifyID.length + " digit(s). This ID is invalid. Insert a valid ten digit ID number").
+    return;
+  } else {
+    $("#memberIDSearchResult").css("display","block")
+    if (membersIDs.indexOf(Number(verifyID)) < 0){
+      $("#IDmatchManualConfirm").addClass("text-danger")
+      $("#IDmatchManualConfirm").text("No matching ID found.")
+    } else {
+      $("#IDmatchManualConfirm").removeClass("text-danger")
+      $("#IDmatchManualConfirm").addClass("text-success")
+      $("#IDmatchManualConfirm").text("ID found. Confirm Data Below")
+      $("#ManualNameConfirm").css("display","block")
+      $("#ManualNameConfirm").text(clientList[membersIDs.indexOf(Number(verifyID))].Name)
+      $("#ManualDoBConfirm").css("display","block")
+      $("#ManualDoBConfirm").text(clientList[membersIDs.indexOf(Number(verifyID))].DoB)
+  
+    }
+  }
 });
 
 
